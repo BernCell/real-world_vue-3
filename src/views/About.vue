74e4f50e9a2f6efb662afb4caf64198d5a37c4ae
BernCell <bern.cell@hotmail.com>
2022-02-01 02:02:04 +0100

    L3-ending

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
+  <div class="about-description">
     <h1>This is an about page</h1>
+     <!-- <span>@{{ event.time }} on {{ event.date }}</span>-->
+    <!-- <h3>{{ event.description }}</h3>  -->
+    <!-- <EventCard  :key='event.id' :event='event' /> -->
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+  // props: {
+  //   event: Object,
+  // },
+  
+};
+</script>
+
+<style>
+.about-description {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
